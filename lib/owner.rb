9766386f5f21b6ee27ffab0e16bd3eb537deeb5d
Wendy Harris <wendyv510@hotmail.com>
2019-11-14 18:28:59 +0000

    Automatically backed up by Learn

--- lib/owner.rb
+++ lib/owner.rb
@@ -64,7 +64,7 @@ class Owner
   end 
   
   def list_pets 
-    "I have #{self.dogs} #{self.cats}"
+    " #{self.dogs} #{self.cats}"
     #self.cats.select {|c| c.owner}
     #self.dogs.select {|d| d.owner}
   end 
